e0a1a521313357759d41bc932e93b2736bd998a8
Breno Alves <breno550@hotmail.com>
2020-05-24 03:22:54 -0300

    auth middleware test refactoring

--- __tests__/middlewares/auth.test.js
+++ __tests__/middlewares/auth.test.js
@@ -1,31 +1,35 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import factory from '../util/factories';
+
 describe('Auth', () => {
   it('should auth and receive token', async () => {
-    const response = await request(app).post('/user/').send({
-      name: 'test gametask auth',
-      email: 'gametaskauth@gametask.com',
-      birthday: '10/11/1995',
-      password: 'test123',
+    const user = await factory.attrs('User');
+    await request(app).post('/user').send(user);
+
+    const response = await request(app).post('/user/auth').send({
+      email: user.email,
+      password: user.password,
     });
 
     expect(response.body).toHaveProperty('token');
   });
 
   it('should receive no token provided', async () => {
-    const response = await request(app).get('/user/').send({
-      email: 'gametaskauth@gametask.com',
-      password: 'test123',
-    });
+    const user = await factory.attrs('User');
+    const response = await request(app).get('/user/').send(user);
 
     expect(response.body).toEqual({ error: 'No token provided' });
   });
 
   it('should receive invalid token', async () => {
+    const user = await factory.attrs('User');
+    await request(app).post('/user').send(user);
+
     const auth_response = await request(app).post('/user/auth').send({
-      email: 'gametask@gametask.com',
-      password: 'test123',
+      email: user.email,
+      password: user.password,
     });
 
     const { token } = auth_response.body;
@@ -34,17 +38,20 @@ describe('Auth', () => {
       .get('/user/')
       .set('Authorization', `Bearer ${token}` + 'l')
       .send({
-        email: 'gametaskauth@gametask.com',
-        password: 'test123',
+        email: user.email,
+        password: user.password,
       });
 
     expect(response.body).toEqual({ error: 'Invalid token' });
   });
 
   it('should receive token malformatted', async () => {
+    const user = await factory.attrs('User');
+    await request(app).post('/user').send(user);
+
     const auth_response = await request(app).post('/user/auth').send({
-      email: 'gametask@gametask.com',
-      password: 'test123',
+      email: user.email,
+      password: user.password,
     });
 
     const { token } = auth_response.body;
@@ -53,8 +60,8 @@ describe('Auth', () => {
       .get('/user/')
       .set('Authorization', `Bearer${token}`)
       .send({
-        email: 'gametaskauth@gametask.com',
-        password: 'test123',
+        email: user.email,
+        password: user.password,
       });
 
     expect(response.body).toEqual({ error: 'Token malformatted' });
